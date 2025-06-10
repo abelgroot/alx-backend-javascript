43e4502af443bd6516b80e756044628953cb7a94
groot <abel.mulugeta.dagne@gmail.com>
2025-06-10 22:40:59 +0300

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+// Return a promise that resolves or rejects based on the boolean input
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+
+module.exports = getFullResponseFromAPI;
