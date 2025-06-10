c5d7bb7b7a1946e270739b0a4b50f80e9baad8cb
groot <abel.mulugeta.dagne@gmail.com>
2025-06-11 00:26:35 +0300

    4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,8 +3,8 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
   res.end('Hello ALX!');
 });
 
