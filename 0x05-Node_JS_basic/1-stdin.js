fd8bd01cbb0cb2890541090f55571de2f2616cae
groot <abel.mulugeta.dagne@gmail.com>
2025-06-11 22:55:22 +0300

    1-stdin.js updated last

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,19 @@
+#!/usr/bin/env node
+// Reads user name from stdin and prints a greeting
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  if (name) {
-    console.log(`Your name is: ${name}`);
+  const name = data.trim();
+  process.stdout.write(`Your name is: ${name}`);
+  if (process.stdin.isTTY) {
+    process.stdout.write('\n');
+    process.exit(0);
   }
 });
 
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit();
 });
