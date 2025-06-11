1c4bd7d08682981dd73d4618c832fe65e6318f8a
groot <abel.mulugeta.dagne@gmail.com>
2025-06-11 23:26:12 +0300

    6-http_express.js updated

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,9 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');  
+  res.send('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(1245, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server listening on port 1245');
+});
 
 module.exports = app;
