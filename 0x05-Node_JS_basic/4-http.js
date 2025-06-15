f20d2e4e4a262f4c5848ed0ce2efc68cef914a9d
groot <abel.mulugeta.dagne@gmail.com>
2025-06-15 23:37:18 +0300

    4-http

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,9 @@
 const http = require('http');
 
-// Create HTTP server
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-// Listen on port 1245
-app.listen(1245);
+app.listen(1245, 'localhost');
 
 module.exports = app;
